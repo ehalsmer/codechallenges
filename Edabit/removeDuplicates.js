98987be843df1f7fb373499210d133392e11a462
Eleasah Halsmer <eleasahjh@gmail.com>
2019-09-29 20:29:05 -0600

    function to remove duplicates from an array, using Set()

---
+++ Edabit/removeDuplicates.js
@@ -0,0 +1,18 @@
+/*
+Create a function that takes an array of items, removes all duplicate items and returns a new array in the same sequential order as the old array (minus duplicates).
+
+Examples
+removeDups([1, 0, 1, 0]) ➞ [1, 0]
+
+removeDups(["The", "big", "cat"]) ➞ ["The", "big", "cat"]
+
+removeDups(["John", "Taylor", "John"]) ➞ ["John", "Taylor"]
+Notes
+Tests contain arrays with both strings and numbers.
+Tests are case sensitive.
+*/
+
+function removeDups(arr){
+    return [...new Set(arr)];
+}
+console.log(removeDups([1, 4, 4, 3]))
